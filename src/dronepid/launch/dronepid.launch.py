e987c93126f32f652333dcbc2efdc4e4b1c2a5d2
Yubo Wang <yubo0809@outlook.com>
2024-02-18 05:05:38 -0500

    undid bad commit

--- src/dronepid/launch/dronepid.launch.py
+++ src/dronepid/launch/dronepid.launch.py
@@ -7,7 +7,7 @@ from launch_ros.actions import Node
 def generate_launch_description():
     ld = LaunchDescription()
     config_path = os.path.join(
-        get_package_share_directory("dronepid"), "config", "config.yaml"
+        get_package_share_directory("dronepid"), "config.yaml"
     )
 
     dronepid_node = Node(
